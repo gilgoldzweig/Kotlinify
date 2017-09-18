589cf4181a11293943307571ad0888d9aab33432
gilgoldzweig <gil5841@gmail.com>
2017-09-18 13:27:41 +0300

    changed uppercase names of modules

--- app/src/main/java/goldzweigapps/com/extentions/MainActivity.kt
+++ app/src/main/java/goldzweigapps/com/extentions/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import goldzweigapps.com.core.bundles.bundle
+import goldzweigapps.com.core.bundles.contains
+import goldzweigapps.com.core.preferences.GlobalSharedPreferences
+import goldzweigapps.com.core.preferences.pref
+import goldzweigapps.com.core.support.fragment
+import goldzweigapps.com.core.threads.*
+import goldzweigapps.com.jackson.fromJson
+import goldzweigapps.com.jackson.toJson
+import goldzweigapps.com.jackson.toPrettyJson
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -41,7 +50,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         isUiThread()
-        GlobalSharedPreferences.initialize(this)
+        GlobalSharedPreferences.initialize(application, "")
+
 
         pref {
             //put key(String), value(Any)
@@ -63,7 +73,7 @@ class MainActivity : AppCompatActivity() {
 
         }
         "key" in GlobalSharedPreferences
-
+        (GlobalSharedPreferences - "").apply()
         val bundle = bundle {
             //put key(String), value(Any)
             //String
@@ -75,11 +85,11 @@ class MainActivity : AppCompatActivity() {
             //Float
             put("key", 1.5131F)
             //parcelable
-            put("key", Parcelable)
+            put("key", Parcelable) //Some parcelable object
             //parcelable array
-            put("key", Array<Parcelable>())
+            put("key", Array<Parcelable>()) //Some parcelable Array
             //parcelable arrayList
-            put("key", ArrayList<Parcelable>())
+            put("key", ArrayList<Parcelable>())//Some parcelable ArrayList
 
             "key" += 5
             this += "keyPair" to 6L
